559cc10804382b43d480064330c18ba17321cb58
imbator <zakhar@fishandfly.ru>
2022-10-01 17:04:32 +0300

    initialized / ALU added
    initialized / ALU added

---
+++ ultimate_processor.srcs/sources_1/new/ALU_wrapper.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 27.09.2022 15:50:28
+// Design Name: 
+// Module Name: ALU_wrapper
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ALU_wrapper(
+    );
+endmodule
